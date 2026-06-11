e1846db1eb0195729cdfff171a24a1764b4f0a58
Mar <marloes.s@outlook.com>
2026-06-11 16:03:08 +0200

    Higher lambda_ch exp 2.0

--- runner.py
+++ runner.py
@@ -298,7 +298,18 @@ def createSpecificExp():
         
     return pool
 
+def createSomeExp():
+    pool = Pool()
+    for seed in [1,2,3]:
+        conf = ConfigBuilder().with_seed(seed).with_max_eat(7).with_lambda_chemokine([[0, 250]]).with_savepath("img/some")
+        name = f"some_s{seed}_e7_l250"
+        conf = conf.with_config_name(Path(f"conf_{name}.json")).with_expname(name).build_and_save()
+        experiment = Experiment().with_config_path(conf.get_full_path()).with_js_path(JSExperimentPaths.SINGLE_CH)
+        experiment = experiment.with_output_file(Path(f"results/some/exp_{name}.txt"))
+        pool.add(experiment)
+    return pool
+
 # Remove below to define own experiments
-pool = createSpecificExp()
+pool = createSomeExp()
 pool.spawn_all()
 pool.await_all()
